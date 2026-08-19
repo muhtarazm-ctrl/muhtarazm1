9601157b9cc82ef0bc84eb66536ad9f1d8b59bb1
اسمك هنا <m.ziad.designer@gmail.com>
2026-08-19 20:31:26 +0300

    fix(render): enforce force-dynamic on root layout and set staticPageGenerationTimeout in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // التجاوز القسري لمعالجة الصفحات أثناء البناء
+  staticPageGenerationTimeout: 1000,
 };
 
 module.exports = nextConfig;
